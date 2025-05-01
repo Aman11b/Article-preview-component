ddf5720b7aaf17aec0ee0d83ab026569a3abfb42
Aman Singh <amansingh1100b@gmail.com>
2025-05-01 21:18:50 +0000

    Js is correct need design changement of overlay

--- main.js
+++ main.js
@@ -3,16 +3,21 @@ const overlaySection = document.getElementById ( 'share-Overlay')
 
 const shareButton =document.getElementById('share-Button');
 const shareOverlay = document.getElementById('share-Button-overlay');
+
+function isMobileView (){
+    return window.innerWidth <=375;
+}
+
 shareButton.addEventListener('click',()=>{
-    if(overlaySection.style.display === 'none'){
-        overlaySection.style.display = '';
-        profileSection.style.display = 'none';
+    if(isMobileView()){
+        overlaySection.style.display = overlaySection.style.display === 'none' ? '' : "none";
+        profileSection.style.display = overlaySection.style.display === 'none' ? '' : 'none';
+    }else{
+        overlaySection.style.display = overlaySection.style.display === 'none' ? '' : 'none';
+        profileSection.style.display = '';
     }
 });
-shareOverlay.addEventListener ('click',()=>{
-    if(profileSection.style.display === 'none')
-        {
-            overlaySection.style.display = 'none';
-            profileSection.style.display = '';
-        }
+shareOverlay.addEventListener('click',()=>{
+    overlaySection.style.display = 'none';
+    profileSection.style.display = '';
 });
